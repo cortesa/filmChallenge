6226598daf5236441c6fc939da9fc78f075e5cf1
Andrés Cortés <cortesa@gmail.com>
2025-09-13 23:15:33 +0200

    poster zoom

--- src/components/MovieInfo/index.tsx
+++ src/components/MovieInfo/index.tsx
@@ -9,8 +9,12 @@ import { HeartIcon } from "../Icons/HeartIcon"
 import { useWishList } from "@/state/useWishList"
 
 import "./MovieInfo.scss"
+import { ZoomInIcon, ZoomOutIcon } from "../Icons"
+import { useState } from "react"
+import clsx from "clsx"
 
 export function MovieInfo() {
+	const [fullScreen, setFullScreen] = useState(false)
 	const { mId } = useParams()
 	const { data: movie } = useMovieInfo({ mId: Number(mId) })
 	const {isIn, toggle} = useWishList()
@@ -19,32 +23,42 @@ export function MovieInfo() {
 
 	const statusColor = getStatusColor({ status: movie.status })
 	const genres = movie.genres ?? []
-	const companies = movie.production_companies ?? []
 
 	return (
 		<div className="movie-info_container">
+			<div className={clsx("full-screen", fullScreen ? "show" : "hide")}>
+					<div className="full-poster">
+					<ZoomOutIcon className="zoom out" size={25} onClick={()=>setFullScreen(false)}/>
+						<TmdbImg path={movie.poster_path} />
+					</div>
+			</div>
 			<div className="backdrop">
 				<TmdbImg path={movie.backdrop_path} />
 			</div>
 			<div className="detail">
 				<div className="surface">
 					<div className="poster">
+						<ZoomInIcon className="zoom" size={25} onClick={()=>setFullScreen(true)}/>
 						<TmdbImg path={movie.poster_path} />
 					</div>
-					<div className="info">
-						<h1>{movie.title}</h1>
-						{movie.tagline && (
-							<p className="tagline">“{movie.tagline}”</p>
-						)}
-						<h3>
-							🎬 {movie.release_date} | ⏱️ {minutesToHM({ minutes: movie.runtime })}
-						</h3>
-						<h4>
-							⭐️ {movie.vote_average?.toFixed(1)} / 10{" "}
-							<span>({movie.vote_count} votos)</span>{"  "}
-							<span>🌍 {movie.original_language?.toUpperCase()}</span>
-						</h4>
-						<Pill size="m" color={statusColor} label={movie.status ?? "Unknown"} />
+					{/* <div className="info"> */}
+
+						<div className="info-title">
+							<h1>{movie.title}</h1>
+							{movie.tagline && (
+								<p className="tagline">“{movie.tagline}”</p>
+							)}
+						</div>
+						<div className="info-details">
+							<h3>
+								🎬 {movie.release_date} | ⏱️ {minutesToHM({ minutes: movie.runtime })}
+							</h3>
+							<h4>
+								⭐️ {movie.vote_average?.toFixed(1)} / 10{" "}
+								<span>({movie.vote_count} votos)</span>{"  "}
+								<span>🌍 {movie.original_language?.toUpperCase()}</span>
+							</h4>
+							<Pill size="m" color={statusColor} label={movie.status ?? "Unknown"} />
 						{genres.length > 0 && (
 							<div className="genres">
 								<h5 className="genres-title">Genres</h5>
@@ -62,7 +76,9 @@ export function MovieInfo() {
 								</a>
 							)}
 						</div>
-					</div>
+						</div>
+						
+					{/* </div> */}
 					<p className="desc">{movie.overview}</p>
 					<div className="save" onClick={() => toggle( mId )}>
 						<Pill
